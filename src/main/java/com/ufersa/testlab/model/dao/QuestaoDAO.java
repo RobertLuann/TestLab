657ec72ffdee14154d1952fad98488dc4b5becc2
Renixon Christian <renixon.silva@alunos.ufersa.edu.br>
2025-08-01 11:34:37 -0300

    feat: singleton JPAUtil

--- src/main/java/com/ufersa/testlab/model/dao/QuestaoDAO.java
+++ src/main/java/com/ufersa/testlab/model/dao/QuestaoDAO.java
@@ -59,30 +59,33 @@ public class QuestaoDAO {
     }
 
     public List<Questao> buscarPorFiltros(String disciplina, String assunto, Long dificuldade) {
-        EntityManager em = JPAUtil.getEntityManager();
-        StringBuilder jpql = new StringBuilder("SELECT q FROM Questao q WHERE 1=1");
-        Map<String, Object> params = new HashMap<>();
+        // Use o método auxiliar executeQuery, que já fornece o EntityManager 'em'
+        return jpaUtil.executeQuery(em -> {
+            StringBuilder jpql = new StringBuilder("SELECT q FROM Questao q WHERE 1=1");
+            Map<String, Object> params = new HashMap<>();
 
-        if (disciplina != null && !disciplina.isBlank()) {
-            jpql.append(" AND q.codigoDisciplina LIKE :disciplina");
-            params.put("disciplina", "%" + disciplina + "%");
-        }
-        if (assunto != null && !assunto.isBlank()) {
-            jpql.append(" AND q.assunto LIKE :assunto");
-            params.put("assunto", "%" + assunto + "%");
-        }
-        if (dificuldade != null) {
-            jpql.append(" AND q.dificuldade = :dificuldade");
-            params.put("dificuldade", dificuldade);
-        }
+            if (disciplina != null && !disciplina.isBlank()) {
+                // OBS: Pode ser necessário usar "q.disciplina.codigo" aqui. Veja a dica abaixo.
+                jpql.append(" AND q.disciplina.codigo LIKE :disciplina");
+                params.put("disciplina", "%" + disciplina + "%");
+            }
+            if (assunto != null && !assunto.isBlank()) {
+                jpql.append(" AND q.assunto LIKE :assunto");
+                params.put("assunto", "%" + assunto + "%");
+            }
+            if (dificuldade != null) {
+                jpql.append(" AND q.dificuldade = :dificuldade");
+                params.put("dificuldade", dificuldade);
+            }
 
-        TypedQuery<Questao> query = em.createQuery(jpql.toString(), Questao.class);
+            TypedQuery<Questao> query = em.createQuery(jpql.toString(), Questao.class);
 
-        for (Map.Entry<String, Object> entry : params.entrySet()) {
-            query.setParameter(entry.getKey(), entry.getValue());
-        }
+            for (Map.Entry<String, Object> entry : params.entrySet()) {
+                query.setParameter(entry.getKey(), entry.getValue());
+            }
 
-        return query.getResultList();
+            return query.getResultList();
+        });
     }
 
     public void atualizarQuestao(Questao questao) {
